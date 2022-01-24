d384ab5b5bba2743f99115a2c68187d0db279c4d
juaoantonio <joaobarbosadev@gmail.com>
2022-01-24 12:12:01 -0300

    Decoupled secret key for the local and remote development

--- pypro/settings.py
+++ pypro/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_o+37o*avx#v)b&-yygi^rg49=rkq@owkox495e2ut4ld&z&q+'
+SECRET_KEY = config('SECRET_KEY', cast=str)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
